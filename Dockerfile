7694086c8a9633be7fdf5fa6adbff69aaec3aa49
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2015-02-09 14:30:19 +0200

    + new config + new README + new Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
 FROM node:slim
 MAINTAINER Vitaly Kovalyshyn "v.kovalyshyn@webitel.com"
 
-ENV VERSION 3.0.5013
+ENV VERSION 3.0.5014
 
 COPY src /core
 
-VOLUME ["/core/config", "/core/cert"]
+VOLUME ["/core/cert"]
 
 WORKDIR /core
-ENTRYPOINT ["node", "app.js"]
+RUN npm install && npm cache clear
 
 EXPOSE 10022
+ENTRYPOINT ["/docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+
+if [ "$SSL" ]; then
+	sed -i 's/SSL/'$SSL'/g' /core/config/config.json
+else
+	sed -i 's/SSL/false/g' /core/config/config.json
+fi
+
+if [ "$FS_HOST" ]; then
+	sed -i 's/FS_HOST/'$FS_HOST'/g' /core/config/config.json
+fi
+
+if [ "$MONGODB_HOST" ]; then
+	sed -i 's/MONGODB_HOST/'$MONGODB_HOST'/g' /core/config/config.json
+fi
+
+if [ "$CONSOLE_HOST" ]; then
+	sed -i 's/CONSOLE_HOST/'$CONSOLE_HOST'/g' /core/config/config.json
+fi
+
+if [ "$CONSOLE_PASSWORD" ]; then
+	sed -i 's/CONSOLE_PASSWORD/'$CONSOLE_PASSWORD'/g' /core/config/config.json
+fi
+
+if [ "$LOGLEVEL" ]; then
+	sed -i 's/LOGLEVEL/'$LOGLEVEL'/g' /core/config/config.json
+else
+	sed -i 's/LOGLEVEL/warn/g' /core/config/config.json
+fi
+
+if [ "$LOGSTASH_ENABLE" ]; then
+	sed -i 's/LOGSTASH_ENABLE/'$LOGSTASH_ENABLE'/g' /core/config/config.json
+else
+	sed -i 's/LOGSTASH_ENABLE/false/g' /core/config/config.json
+fi
+
+if [ "$LOGSTASH_HOST" ]; then
+	sed -i 's/LOGSTASH_HOST/'$LOGSTASH_HOST'/g' /core/config/config.json
+fi
+
+if [ "$TOKEN_KEY" ]; then
+	sed -i 's/TOKEN_KEY/'$TOKEN_KEY'/g' /core/config/config.json
+fi
+
+exec node app.js
