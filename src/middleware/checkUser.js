c117f07ecf3fa269e63665ad0fd3757b7be3e8cc
Navrotskyj <navrotskyj@gmail.com>
2015-02-20 14:52:07 +0200

    fix checkUser

--- src/middleware/checkUser.js
+++ src/middleware/checkUser.js
@@ -26,50 +26,7 @@ module.exports = function (login, password, cb) {
             };
             return
         };
-        // TODO Нужна оптимизация, при большых нагрузках ивенты свича залипают. !!!
-        var _loginPlain = login.replace('@', ' ');
-        var parseXmlBody = function (body, login) {
-            var parser = new xml2js.Parser({ explicitArray: false, explicitRoot: false }),
-                headers = {},
-                _body = body;
 
-            try {
-                parser.parseString(body, function (err, data) {
-                    if (err) {
-                        log.debug('Auth error: %s %s', login, _body.replace('\n', ''));
-                        return;
-                    }
-                    ;
-                    var _attr = [].concat(data.params.param, data.variables.variable);
-                    _attr.forEach(function (header) {
-                        headers[header['$'].name] = header['$'].value;
-                    });
-
-                });
-            } catch (e){
-                log.error(e.message);
-            }
-
-            return headers;
-        };
-        eslConn.bgapi('find_user_xml id ' + _loginPlain, function (res) {
-            var _jsonParamUser = parseXmlBody(res['body'], _loginPlain);
-
-            var a1Hash = md5(login.replace('@', ':') + ':' + password);
-            var registered = (a1Hash == _jsonParamUser['a1-hash']);
-
-            if (registered) {
-                cb(null, {
-                    'role': ACCOUNT_ROLE.getRoleFromName(_jsonParamUser['account_role']),
-                    'domain': login.split('@')[1]
-                });
-            } else {
-                cb(((res.body && res.body.indexOf('-ERR no reply\n') == 0)
-                    ? 'user not found'
-                    : 'secret incorrect'));
-            };
-        });
-        /*
         webitel.userDara(login, 'global', ['a1-hash', 'account_role'], function (res) {
             try {
                 var resJson = JSON.parse(res['body']);
@@ -91,7 +48,7 @@ module.exports = function (login, password, cb) {
                 cb('Bad password.');
             };
         });
-        */
+
     } catch (e) {
         cb(e.message || 'Internal server error');
     }
